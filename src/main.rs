3b236bb448845ce5bbbeedf2b7d40087526b6d8e
ZilongYang <zilong.yang@gmail.com>
2024-04-26 23:56:52 +0800

    修改标题文字，标记完成

--- src/main.rs
+++ src/main.rs
@@ -100,6 +100,65 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
+    ui.global::<Logic>().on_set_todo_done({
+        let todos: Arc<RwLock<TodoList>> = Arc::clone(&todos);
+        let models = Arc::clone(&models);
+        move |id, done| {
+            let id = uuid::Uuid::parse_str(&id).unwrap_or_else(|err| {
+                eprintln!("Failed to parse UUID: {}", err);
+                uuid::Uuid::nil()
+            });
+            // let todos = todos_toggle.clone();
+            let mut todos = todos.write().unwrap();
+            let models = models.write().unwrap();
+            match todos.index_of(id) {
+                Some(index) => {
+                    let todo = &mut todos.todos[index];
+                    todo.done = done;
+                    models.insert(index, TodoItemSlint {
+                        id: id.to_string().into(),
+                        content: todo.content.clone().into(),
+                        done: todo.done,
+                    });
+                    models.remove(index + 1);
+                    todos.save_to_ymal_file();
+                }
+                None => {
+                    eprintln!("Failed to find todo with ID: {}", id);
+                }
+            }
+        }
+    });
+
+    ui.global::<Logic>().on_set_todo_content({
+        let todos = Arc::clone(&todos);
+        let models = Arc::clone(&models);
+        move |id, content| {
+            let id = uuid::Uuid::parse_str(&id).unwrap_or_else(|err| {
+                eprintln!("Failed to parse UUID: {}", err);
+                uuid::Uuid::nil()
+            });
+            let mut todos = todos.write().unwrap();
+            let models = models.write().unwrap();
+            match todos.index_of(id) {
+                Some(index) => {
+                    let todo = &mut todos.todos[index];
+                    todo.content = content.clone().into();
+                    models.insert(index, TodoItemSlint {
+                        id: id.to_string().into(),
+                        content: todo.content.clone().into(),
+                        done: todo.done,
+                    });
+                    models.remove(index + 1);
+                    todos.save_to_ymal_file();
+                }
+                None => {
+                    eprintln!("Failed to find todo with ID: {}", id);
+                }
+            }
+        }
+    });
+
     ui.run()?;
     Ok(())
 }
--- src/todo.rs
+++ src/todo.rs
@@ -1,35 +1,23 @@
 use serde::{Deserialize, Serialize};
 use std::env;
-use std::path::{Path, PathBuf};
-use std::{fs, path};
+use std::path::{Path};
+use std::{fs};
 use uuid::Uuid;
 
 pub fn load_todos() -> Vec<TodoItem> {
-    let todos = vec![
-        TodoItem::new("Buy milk".to_string()),
-        TodoItem::new("Feed the cat".to_string()),
-        TodoItem::new("Prepare for the meeting".to_string()),
-    ];
-
-    // 从 YAML 文件加载配置
-    // let mut yaml_content: String = String::new();
-    // match fs::read_to_string("todos.yml") {
-    //     Ok(str) => yaml_content = str,
-    //     Err(_) => {
-    //         println!("Error reading file");
-    //     }
-    // };
 
     // 获取当前程序的运行目录
     let current_dir = env::current_dir().unwrap();
     // 拼接出"todos.yml"的完整路径
     let file_path = Path::new(&current_dir).join("todos.yml");
 
-    let yaml_content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+    let yaml_content = fs::read_to_string(file_path)
+    .unwrap_or_else(|err| {
         eprintln!("Error reading file: {}", err);
         String::new()
     });
-    let todos: Vec<TodoItem> = serde_yaml::from_str(&yaml_content).unwrap_or_else(|err| {
+    let todos: Vec<TodoItem> = serde_yaml::from_str(&yaml_content)
+    .unwrap_or_else(|err| {
         eprintln!("Error parsing YAML: {}", err);
         vec![]
     });
@@ -37,12 +25,14 @@ pub fn load_todos() -> Vec<TodoItem> {
 }
 
 pub fn save_todos(todos: Vec<TodoItem>) {
-    let yaml_content = serde_yaml::to_string(&todos).unwrap_or_else(|err| {
+    let yaml_content = serde_yaml::to_string(&todos)
+    .unwrap_or_else(|err| {
         eprintln!("Error serializing YAML: {}", err);
         String::new()
     });
 
-    let current_dir = env::current_dir().unwrap();
+    let current_dir = env::current_dir()
+    .unwrap();
     let file_path = Path::new(&current_dir).join("todos.yml");
 
     fs::write(file_path, yaml_content).unwrap_or_else(|err| {
@@ -99,6 +89,15 @@ impl IntoIterator for TodoList {
         self.todos.into_iter()
     }
 }
+
+// impl Iterator for TodoList {
+//     type Item = TodoItem;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         self.todos.pop()
+//     }
+    
+// }
 
 impl<'a> IntoIterator for &'a TodoList {
     type Item = &'a TodoItem;
